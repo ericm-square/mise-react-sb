bc1ad7f7b946a0b0c11e6d94f14a6b90c2680834
Morgan Eric <ericm@squareup.com>
2024-12-19 08:27:48 -0600

    Remove mise-react

--- mise-react/src/app/mise/order/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function OrderPage() {
-    return (
-        <div>
-            Order Page
-        </div>
-    );
-}
--- mise-react/src/components/theme/icons/WarningIcon.tsx
+++
@@ -1,8 +0,0 @@
-import { IIconProps } from "../partials/ui/Icon/Icon";
-
-export default function TrashIcon(props: IIconProps) {
-  const { iconClasses } = props;
-  return (
-    <svg className={iconClasses} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="m40-120 440-760 440 760H40Zm138-80h604L480-720 178-200Zm302-40q17 0 28.5-11.5T520-280q0-17-11.5-28.5T480-320q-17 0-28.5 11.5T440-280q0 17 11.5 28.5T480-240Zm-40-120h80v-200h-80v200Zm40-100Z"/></svg>
-  );
-}
